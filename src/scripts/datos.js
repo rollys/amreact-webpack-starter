aa884bac5632e4e4edf94dc1eec6802be351e212
Rolly <rolly.sanchez.26@gmail.com>
2019-11-26 17:22:43 -0500

    import y export de módulos

---
+++ src/scripts/datos.js
@@ -0,0 +1,27 @@
+const misdatos = {
+    nombre: 'Rolly',
+    edad: 28,
+};
+
+//2
+export const datosMama = {
+    nombre: 'Maria',
+    edad: 54,
+};
+
+export const datosPapa = {
+    nombre: 'Antonio',
+    edad: 60,
+};
+
+const anioActual = 2019;
+const mesActual = 'Noviembre';
+
+//3
+export {
+    anioActual,
+    mesActual,
+}; 
+
+//1
+export default misdatos;
---
+++ src/scripts/index.js
@@ -0,0 +1,13 @@
+// import MisDatos from './datos';
+// import {datosMama, datosPapa} from './datos'
+/*import Data, {datosMama as Mama, datosPapa, anioActual, mesActual} from './datos';
+
+console.log('MisDatos', Data);
+console.log('datosMama', Mama);
+console.log('datosPapa', datosPapa);
+*/
+
+import * as Datos from './datos';
+
+console.log(Datos.anioActual);
+console.log(Datos.mesActual);
